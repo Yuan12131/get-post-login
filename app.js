1ea5ce58bdb267033489747eb9cfa981aa32f886
leeyuan <dbdks32123@gmail.com>
2023-10-18 14:41:02 +0900

    서버 구동 확인

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
+http.createServer(function(req, res){
+  res.writeHead(200, {"Content-Type" : "text/html"});
+  res.end("test")
+}).listen(8019)
