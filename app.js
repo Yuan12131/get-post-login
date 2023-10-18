ad0e3e498b524818204944ea99d1d75730d3e56b
leeyuan <dbdks32123@gmail.com>
2023-10-18 18:54:16 +0900

    서버 js 파일에서 유효성 검사 테스트 중

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'); // http 불러오기
+const fs = require('fs'); // fs 불러오기
 
+// 서버 생성
 http.createServer(function(req, res){
 
+  // index.html 불러오기
   if(req.url === '/'){
     fs.readFile("./static/index.html", function(err, data){
       if (err){
@@ -14,6 +16,22 @@ http.createServer(function(req, res){
     })
   }
 
+   // if(req.method === "POST"){
+  //   if (password === passwordTwo){
+  //     user = User.objects.create
+  //   }
+  // }
+
+  // if request.method == 'POST':
+  //       # password와 confirm에 입력된 값이 같다면
+  //       if request.POST['password'] == request.POST['confirm']:
+  //           # user 객체를 새로 생성
+  //           user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+  //           # 로그인 한다
+  //           auth.login(request, user)
+            // return redirect('/')
+
+  // sub.html 불러오기
   if(req.url === '/sub.html'){
     fs.readFile("./static/sub.html", function(err, data){
       if (err){
@@ -25,4 +43,4 @@ http.createServer(function(req, res){
     })
   }
 
-  }).listen(8080)
+  }).listen(8080) //포트 번호 8080
