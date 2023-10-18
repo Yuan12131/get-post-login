8e371e23ddf18255e9cb7822f2033d872c04019d
leeyuan <dbdks32123@gmail.com>
2023-10-18 15:22:59 +0900

    index.html 읽기

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req, res){
+
   res.writeHead(200, {"Content-Type" : "text/html"});
-  res.end("test")
+
+  fs.readFile("./static/index.html", function(err, data){
+    if (err){
+      console.error("파일을 읽지 못했습니다.");
+    } else {
+      res.end(data);
+    }
+  })
 }).listen(8019)
