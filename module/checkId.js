12dc56938158b314240c8073b7fa1e334448b393
leeyuan <dbdks32123@gmail.com>
2023-10-19 19:26:04 +0900

    아이디 영어대소문자 포함 여부 검사 함수 모듈

--- module/checkId.js
+++ module/checkId.js
@@ -1,29 +1,15 @@
-function idCheck(id){
-    // isAlphaNumeric(str) 사용자 정의 함수는 유니코드로 해당값을 검사해줌. 
-    if(!checkEnglish(id)){
-      return false;
-    } else if(id == null){
-      return false;
+function checkId(id) {
+  let upper = false; // 대문자
+  let lower = false; // 소문자
+
+  for (let i = 0; i < id.length; i++) {
+    if (id.charCodeAt(i) >= 65 && id.charCodeAt(i) <= 90) {
+      upper = true; // 대문자가 포함되면 true
+    } else if (id.charCodeAt(i) >= 97 && id.charCodeAt(i) <= 122) {
+      lower = true; // 소문자가 포함되면 true
     }
-    // 조건 만족 후 true 반환
-    return true;
+  }
+  return upper && lower; // 대소문자가 모두 포함되면 true, 아니면 false
 }
 
-function checkEnglish(id){
-  let count = 0;
-  for(let i = 0; i < id.length; i++){
-      if((id.charCodeAt(i)>=65 && id.charCodeAt(i)<=90) && (id.charCodeAt(i)>=97 && id.charCodeAt(i)<=122)){
-          count += 1;
-      }
-  }
-
-  //카운트 수와 문자의 길이가 같다면 true
-  if(count === (id.length)){
-    return true;
-  } else{
-    return false;
-  }
-}
-
-
-module.exports = idCheck;
+module.exports = checkId;
