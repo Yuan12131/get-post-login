b821009cf51802c5077484fbf149879886539034
leeyuan <dbdks32123@gmail.com>
2023-10-19 17:37:54 +0900

    email 유효성 검사 함수의 조건문 오류 없도록 수정

--- module/emailCheck.js
+++ module/emailCheck.js
@@ -1,15 +1,11 @@
 function emailCheck(email){
   // indexOf 함수를 통해 해당 문자(@, .)을 찾기
   // @가 없을 시 유효성 검사에 어긋남
-  if (email.value.indexOf('@') === -1){
-    alert("이메일 형식을 확인해주세요");
-    email.focus();
+  if (email.indexOf('@') === -1){
     return false;
   }
   // '.'이 없을 시 유효성 검사에 어긋남
-  if (email.value.indexOf('.') === -1){
-    alert("이메일 형식을 확인해주세요");
-    email.focus();
+  if (email.indexOf('.') === -1){
     return false;
   }
   else {
