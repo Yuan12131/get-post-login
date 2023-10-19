b24259f78e628db991ed14c940002f7bc7a3e661
leeyuan <dbdks32123@gmail.com>
2023-10-19 19:34:42 +0900

    함수 수정 : .이 @ 뒤에 있는지를 확인

--- module/checkEmail.js
+++ module/checkEmail.js
@@ -1,11 +1,11 @@
 function emailCheck(email){
-  // indexOf 함수를 통해 해당 문자(@, .)을 찾기
-  // @가 없을 시 유효성 검사에 어긋남
+  let atSignIndex = email.indexOf("@");
+// @가 없으면 -1이니 값이 -1이면 fasle
   if (email.indexOf('@') === -1){
     return false;
   }
-  // '.'이 없을 시 유효성 검사에 어긋남
-  if (email.indexOf('.') === -1){
+// .이 @보다 뒤에 있지 않으면 false
+  if (!email.includes('.', atSignIndex)){
     return false;
   }
   else {
