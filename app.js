054a1671ac483715ab6df1265003d14b78e7c951
leeyuan <dbdks32123@gmail.com>
2023-10-19 12:54:54 +0900

    queryString 데이터 함수 테스트중

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 const http = require('http'); // http 불러오기
 const fs = require('fs'); // fs 불러오기
 
+const qs = require("querystring"); // queryString 불러오기
+const signUpAsset = require("./signup-asset/signup-asset.js"); // signUp 데이터 객체 모듈 불러오기
+
 // 서버 생성
 http.createServer(function(req, res){
-
   // index.html 불러오기
-  if(req.url === '/'){
+  if(req.method === 'GET' && req.url === '/'){
     fs.readFile("./static/index.html", function(err, data){
       if (err){
         console.error("파일을 읽지 못했습니다.");
       } else {
         res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end(data);
-      } 
+        res.end(data); 
+      }
     })
+  } 
+
+  // POST 방식으로 요청 (sign up에서 submit)할 때 실행할 함수
+  if (req.method === "POST") {
+    let body = '';
+
+    // data 이벤트가 발생하면 chunk 함수 실행
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // body = body + chunk.toString -> input에 입력된 데이터를 문자열로 body에 반환
+    });
+
+    // end 이벤트가 발생하면 실행되는 함수
+    // on에서 읽어와서 body에 저장한 chunk 데이터를 파싱
+    req.on("end", () => {
+      const parseBody = qs.parse(body);
+      Object.assign(signUpAsset, parseBody); // parseBody의 프로퍼티 키와 동일한 signUpAsset의 프로퍼티 키에 값을 대입
+      console.log(parseBody);
+      console.log(signUpAsset);
+      console.log(signUpAsset.id);
+    });
   }
-
-   // if(req.method === "POST"){
-  //   if (password === passwordTwo){
-  //     user = User.objects.create
-  //   }
-  // }
-
-  // if request.method == 'POST':
-  //       # password와 confirm에 입력된 값이 같다면
-  //       if request.POST['password'] == request.POST['confirm']:
-  //           # user 객체를 새로 생성
-  //           user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
-  //           # 로그인 한다
-  //           auth.login(request, user)
-            // return redirect('/')
 
   // sub.html 불러오기
   if(req.url === '/sub.html'){
